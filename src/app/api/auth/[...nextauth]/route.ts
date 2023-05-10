98c30bcad8dd0eb740f4be3c22657971afd2ff66
Peter Lee <PeterLee2094@gmail.com>
2023-05-10 14:02:46 -0500

    added next auth credentials

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,17 @@
 import NextAuth from "next-auth/next";
 import type { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import { CredentialsProvider } from "next-auth/providers";
+import Credentials from "next-auth/providers/credentials";
+import {
+  CredentialsProvider,
+  CredentialsProviderType,
+} from "next-auth/providers/credentials";
 import * as dotenv from "dotenv";
 dotenv.config();
+
+import { comparePassword } from "../../../../../lib/encrypt";
+
+// type credentials = { email: string; password: string };
 
 export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
@@ -11,6 +19,41 @@ export const authOptions: NextAuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
+    }),
+
+    Credentials({
+      name: "credentials",
+      credentials: {},
+      async authorize(credentials) {
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
+
+        // fine user
+        const isUser: any = await prisma.user.findFirst({
+          where: {
+            email: email,
+          },
+        });
+
+        if (!isUser) {
+          await prisma.$disconnect();
+          throw new Error("No user found");
+        }
+
+        // compare password
+        const isPassword = await comparePassword(password, isUser.password);
+
+        if (!isPassword) {
+          await prisma.$disconnect();
+          throw new Error("Could not log you in");
+        }
+
+        // disconnect to db
+        await prisma.$disconnect();
+        return { email: isUser.email } as any;
+      },
     }),
   ],
 };
