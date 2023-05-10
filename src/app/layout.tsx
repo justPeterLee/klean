63f6a14fdbbfdaa724a75ddf5e0f0776de8a4e1e
Peter Lee <PeterLee2094@gmail.com>
2023-05-10 13:12:34 -0500

    added authenication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 
+import AuthProvider from "./AuthProvider";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -14,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </AuthProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import styles from "./page.module.css";
-
+import {
+  SignInButton,
+  SignOutButton,
+} from "@/components/AuthButtons/AuthButtons";
 export default function Home() {
   return (
     <main className={styles.main}>
       <p>hello</p>
+      <SignInButton />
+      <SignOutButton />
     </main>
   );
 }
