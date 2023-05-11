06b04adec253c1ce2c82fa5d8821f8fa279976d8
Peter Lee <PeterLee2094@gmail.com>
2023-05-11 16:37:40 -0500

    created and imported navbar to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 
 import AuthProvider from "./AuthProvider";
-
+import Navbar from "@/components/Navbar/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -18,7 +18,9 @@ export default function RootLayout({
   return (
     <AuthProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Navbar /> {children}
+        </body>
       </html>
     </AuthProvider>
   );
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+"use client";
+import styles from "./Navbar.module.css";
+import Link from "next/link";
+export default function Navbar() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logoContainer}>
+        <Link href={"/"}>Logo</Link>
+      </div>
+      <div className={styles.linkContainer}>
+        <Link href={"/shop"}>shop</Link>
+        <Link href={"/about"}>about</Link>
+        <Link href={"/contact"}>contact</Link>
+      </div>
+      <div className={styles.iconContainer}>
+        <Link href={"/user"}></Link>
+        <button className={styles.cartButton}> cart </button>
+      </div>
+    </div>
+  );
+}
