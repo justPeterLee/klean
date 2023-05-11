edbfa52c7abbef2d8d0f0ac09d2d393ee842e5ad
Peter Lee <PeterLee2094@gmail.com>
2023-05-11 17:17:40 -0500

    add responsive navbar on page scroll

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,29 @@
 "use client";
+import { useState, useEffect } from "react";
 import styles from "./Navbar.module.css";
 import Link from "next/link";
 export default function Navbar() {
+  let lastScroll = 0;
+  const [topStyle, setTopStyle] = useState<string>();
+
+  const pageScroll = (e: any) => {
+    let currentScroll: number = window.pageYOffset;
+
+    if (currentScroll - lastScroll < 0) {
+      setTopStyle("0px");
+    } else {
+      setTopStyle("-100px");
+    }
+
+    lastScroll = currentScroll;
+  };
+  useEffect(() => {
+    // setPrevScrollpos(window.pageYOffset);
+    window.addEventListener("scroll", pageScroll, { passive: true });
+  }, []);
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{ top: topStyle }}>
       <div className={styles.subContainer}>
         <div className={styles.logoContainer}>
           <Link href={"/"}>Logo</Link>
