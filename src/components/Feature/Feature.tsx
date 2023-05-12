3f857f25bccff66ff70e13d446bf31ad1631fb4f
Peter Lee <PeterLee2094@gmail.com>
2023-05-11 20:34:54 -0500

    added menu option styling

--- src/components/Feature/Feature.tsx
+++ src/components/Feature/Feature.tsx
@@ -2,9 +2,13 @@
 import styles from "./Feature.module.css";
 import Menu from "./Menu/Menu";
 export function MainFeature() {
+  const readOption = (option: string) => {
+    console.log(option);
+  };
+
   return (
     <div className={styles.MainFeatureContainer}>
-      <Menu />
+      <Menu options={["color1", "color2"]} readOption={readOption} />
       <button className={styles.viewProductButton}>view product</button>
     </div>
   );
