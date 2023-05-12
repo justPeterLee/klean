d5c44bdf69892c4ce49b2501d2c9d723213c9d85
Peter Lee <PeterLee2094@gmail.com>
2023-05-11 20:47:36 -0500

    added custom styling to menu

--- src/components/Feature/Menu/Menu.tsx
+++ src/components/Feature/Menu/Menu.tsx
@@ -3,18 +3,38 @@ import styles from "./Menu.module.css";
 interface MenuProps {
   options: string[];
   readOption: (param: string) => void;
+
+  containerStyle?: {
+    height?: string;
+    width?: string;
+    top?: string;
+    left?: string;
+    borderRadius?: string;
+  };
+
+  optionStyle?: {
+    height?: string;
+    width?: string;
+    backgroundColor?: string;
+    fontSize?: string;
+    color?: string;
+  };
 }
 export default function Menu(props: MenuProps) {
-  const { options, readOption } = props;
+  const { options, readOption, containerStyle, optionStyle } = props;
   const sendOption = (selectedOption: string) => {
     readOption(selectedOption);
   };
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      style={containerStyle ? containerStyle : {}}
+    >
       {options &&
         options.map((option, index) => (
           <button
             className={styles.menuButton}
+            style={optionStyle ? optionStyle : {}}
             key={index}
             onClick={() => {
               sendOption(option);
