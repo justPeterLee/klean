f1d64f8bda68a653f760ca7d5b295d05307cf60f
Peter Lee <PeterLee2094@gmail.com>
2023-05-11 19:56:46 -0500

    added styling to main feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import styles from "../styling/page.module.css";
 
+import { MainFeature } from "@/components/Feature/Feature";
 export default function Home() {
   return (
     <main className={styles.main}>
       <p>hello</p>
+      <MainFeature />
     </main>
   );
 }
