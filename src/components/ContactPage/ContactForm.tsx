752f4780ced19e557340cff77d0c6513abc13b45
Peter Lee <PeterLee2094@gmail.com>
2023-05-13 22:52:29 -0500

    added contact form error handling

--- src/components/ContactPage/ContactForm.tsx
+++ src/components/ContactPage/ContactForm.tsx
@@ -1,20 +1,29 @@
 "use client";
 import styles from "../../styling/Contact.module.css";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface inputValidationProps {
   valueName: string;
   errorMessage: string;
+  triggerError: boolean;
   sendValue: (param: string) => void;
+  width?: {
+    width?: string;
+  };
 }
 function InputValidation(props: inputValidationProps) {
-  const { valueName, errorMessage, sendValue } = props;
+  const { valueName, errorMessage, triggerError, sendValue, width } = props;
   const [inputValue, setInputValue] = useState("");
   const [error, setError] = useState({ [valueName]: true });
 
+  useEffect(() => {
+    if (!triggerError) {
+      setError({ [valueName]: false });
+    }
+  }, [triggerError]);
   return (
-    <span className={styles.inputContainer}>
+    <span className={styles.inputContainer} style={width}>
       <input
         className={`${styles.input} ${!error[valueName] && styles.inputError}`}
         id={`${valueName}`}
@@ -48,11 +57,20 @@ function InputValidation(props: inputValidationProps) {
   );
 }
 
+interface ProxyError {
+  first: boolean;
+  last: boolean;
+  email: boolean;
+  order: boolean;
+  subject: boolean;
+  message: boolean;
+}
 export function ContactForm() {
   const [error, setError] = useState({
     first: true,
     last: true,
     email: true,
+    order: true,
     subject: true,
     message: true,
   });
@@ -63,16 +81,69 @@ export function ContactForm() {
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
 
+  const sendMessage = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    let proxyErrorObj: ProxyError = error;
+
+    if (!first.replace(/\s/g, "")) {
+      proxyErrorObj = { ...proxyErrorObj, first: false };
+      setError({ ...proxyErrorObj, first: false });
+    } else {
+      proxyErrorObj = { ...proxyErrorObj, first: true };
+      setError({ ...proxyErrorObj, first: true });
+    }
+
+    console.log(error);
+
+    if (!last.replace(/\s/g, "")) {
+      proxyErrorObj = { ...proxyErrorObj, last: false };
+      setError({ ...proxyErrorObj, last: false });
+    } else {
+      proxyErrorObj = { ...proxyErrorObj, last: true };
+      setError({ ...proxyErrorObj, last: true });
+    }
+
+    if (!email.replace(/\s/g, "")) {
+      proxyErrorObj = { ...proxyErrorObj, email: false };
+      setError({ ...proxyErrorObj, email: false });
+    } else {
+      proxyErrorObj = { ...proxyErrorObj, email: true };
+      setError({ ...error, email: true });
+    }
+
+    if (!order.replace(/\s/g, "")) {
+      proxyErrorObj = { ...proxyErrorObj, order: false };
+      setError({ ...proxyErrorObj, order: false });
+    } else {
+      proxyErrorObj = { ...proxyErrorObj, order: true };
+      setError({ ...proxyErrorObj, order: true });
+    }
+
+    if (!subject.replace(/\s/g, "")) {
+      proxyErrorObj = { ...proxyErrorObj, subject: false };
+      setError({ ...proxyErrorObj, subject: false });
+    } else {
+      proxyErrorObj = { ...proxyErrorObj, subject: true };
+      setError({ ...proxyErrorObj, subject: true });
+    }
+
+    if (!message.replace(/\s/g, "")) {
+      proxyErrorObj = { ...proxyErrorObj, message: false };
+      setError({ ...proxyErrorObj, message: false });
+    } else {
+      proxyErrorObj = { ...proxyErrorObj, message: true };
+      setError({ ...proxyErrorObj, message: true });
+    }
+  };
   return (
     <div className={styles.ContactFormContainer}>
-      <form className={styles.formContainer}>
+      <form className={styles.formContainer} onSubmit={sendMessage}>
         <div className={styles.nameContainer}>
-          <p>
-            {first} {last}
-          </p>
           {/* first name */}
           <InputValidation
             valueName="First"
+            triggerError={error.first}
             errorMessage="must include first name"
             sendValue={(value) => {
               setFirst(value);
@@ -82,6 +153,7 @@ export function ContactForm() {
           {/* last name */}
           <InputValidation
             valueName="Last"
+            triggerError={error.last}
             errorMessage="must include last name"
             sendValue={(value) => {
               setLast(value);
@@ -89,28 +161,50 @@ export function ContactForm() {
           />
         </div>
 
-        <div>
-          <span>
-            <label>Email</label>
-            <input />
-          </span>
+        <div className={styles.nameContainer}>
+          {/* email */}
+          <InputValidation
+            valueName="Email"
+            triggerError={error.email}
+            errorMessage="must include your email"
+            sendValue={(value) => {
+              setFirst(value);
+            }}
+          />
 
-          <span>
-            <label>Order ID</label>
-            <input />
-          </span>
+          {/* order id */}
+          <InputValidation
+            valueName="Order ID"
+            triggerError={error.order}
+            errorMessage="must include order id"
+            sendValue={(value) => {
+              setLast(value);
+            }}
+          />
         </div>
 
-        <div>
-          <span>
-            <label>Subject</label>
-            <input />
-          </span>
+        <div className={styles.contentContainer}>
+          {/* subject */}
+          <InputValidation
+            valueName="Subject"
+            triggerError={error.subject}
+            errorMessage="must include subjet"
+            sendValue={(value) => {
+              setSubject(value);
+            }}
+            width={{ width: "33.5rem" }}
+          />
 
-          <span>
-            <label>Message</label>
-            <input />
-          </span>
+          {/* subject */}
+          <InputValidation
+            valueName="Message"
+            triggerError={error.message}
+            errorMessage="must include message"
+            sendValue={(value) => {
+              setMessage(value);
+            }}
+            width={{ width: "33.5rem" }}
+          />
         </div>
 
         <button type="submit">submit</button>
