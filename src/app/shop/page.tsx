6341e48c4a35a2bbf49946295ca82db75a66792e
Peter Lee <PeterLee2094@gmail.com>
2023-05-17 19:36:06 -0500

    added data to store menu

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,9 +1,15 @@
 import styles from "../../styling/Shop.module.css";
+import { ShopMenu } from "@/components/ShopPage/ShopComponents";
 
+const categories = [
+  { category: "Computer Mouse", link: "/computer-mouse" },
+  { category: "Mechanical Keyboard", link: "/mechanical-keyboard" },
+];
 export default async function Shop() {
   return (
     <main className={styles.main}>
       <p>shop</p>
+      <ShopMenu categories={categories} />
     </main>
   );
 }
--- src/components/ShopPage/ShopComponents.tsx
+++ src/components/ShopPage/ShopComponents.tsx
@@ -2,16 +2,16 @@ import { category } from "@prisma/client";
 import styles from "../../styling/Shop.module.css";
 
 interface ShopMenuProps {
-  categories: [{ category: string; link: string }];
+  categories: { category: string; link: string }[];
 }
 
-export default function ShopMenu(props: ShopMenuProps) {
+export function ShopMenu(props: ShopMenuProps) {
   const { categories } = props;
 
   return (
     <span className={styles.MenuContainer}>
       {categories.map((category, index) => (
-        <p key={index}>{category.category}</p>
+        <button key={index}>{category.category}</button>
       ))}
     </span>
   );
