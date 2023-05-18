01f4fefef8d8ff4c1447f959a780016bce09ae0c
Peter Lee <PeterLee2094@gmail.com>
2023-05-17 19:22:04 -0500

    created shop page

---
+++ src/app/shop/page.tsx
@@ -0,0 +1,9 @@
+import styles from "../../styling/Shop.module.css";
+
+export default async function Shop() {
+  return (
+    <main className={styles.main}>
+      <p>shop</p>
+    </main>
+  );
+}
