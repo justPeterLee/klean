6128e5a30239b54495c956ea8f7d6efeaf441c64
Peter Lee <PeterLee2094@gmail.com>
2023-05-17 19:58:34 -0500

    added navigation to shop menu

--- src/components/ShopPage/ShopComponents.tsx
+++ src/components/ShopPage/ShopComponents.tsx
@@ -1,16 +1,23 @@
+"use client";
 import styles from "../../styling/Shop.module.css";
-
+import { useRouter } from "next/navigation";
 interface ShopMenuProps {
   categories: { category: string; link: string; amount: number }[];
 }
 
 export function ShopMenu(props: ShopMenuProps) {
   const { categories } = props;
+  const router = useRouter();
 
   return (
     <span className={styles.MenuContainer}>
       {categories.map((category, index) => (
-        <button key={index}>
+        <button
+          key={index}
+          onClick={() => {
+            router.push(`/shop/${category.link}`);
+          }}
+        >
           {category.category} ({category.amount})
         </button>
       ))}
