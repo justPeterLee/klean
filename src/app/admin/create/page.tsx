c61476dd805337088394e260c4e04c8f720c465e
Peter Lee <PeterLee2094@gmail.com>
2023-05-18 13:23:05 -0500

    created create route and add navigation

---
+++ src/app/admin/create/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import styles from "../../../styling/Admin.module.css";
+import Link from "next/link";
+export default function AdminCreate() {
+  return (
+    <div className={styles.main}>
+      <p>create</p>
+    </div>
+  );
+}
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,10 @@
 import styles from "../../styling/Admin.module.css";
+import Link from "next/link";
 
 export default async function Admin() {
-  return <div className={styles.main}> admin </div>;
+  return (
+    <div className={styles.main}>
+      <Link href={"/admin/create"}>create</Link>
+    </div>
+  );
 }
