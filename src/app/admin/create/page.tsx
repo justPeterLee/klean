f4b2b7b11d1edf8329fc316962fe0fe765c9482b
Peter Lee <PeterLee2094@gmail.com>
2023-05-19 00:04:16 -0500

    added description input

--- src/app/admin/create/page.tsx
+++ src/app/admin/create/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import styles from "../../../styling/Admin.module.css";
 import Link from "next/link";
+import { CreateForm } from "@/components/AdminPage/AdminComponents";
 export default function AdminCreate() {
   return (
     <div className={styles.main}>
       <p>create</p>
+      <CreateForm />
     </div>
   );
 }
