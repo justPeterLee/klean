97f96c1ef2dd679701405fb7d0c9908a23f3ca8c
Peter Lee <PeterLee2094@gmail.com>
2023-06-03 21:28:16 -0500

    dynamically render categories on shop

--- src/app/shop/layout.tsx
+++ src/app/shop/layout.tsx
@@ -1,19 +1,43 @@
 import styles from "../../styling/Shop.module.css";
 import { ShopMenu } from "@/components/ShopPage/ShopComponents";
 
-const categories = [
-  { category: "Computer Mouse", link: "computer-mouse", amount: 5 },
-  { category: "Mechanical Keyboard", link: "mechanical-keyboard", amount: 1 },
-];
+import prisma from "../../../lib/db";
 
-export default function ShopLayout({
+async function fetchCategories() {
+  const responseCategories = await prisma.category.findMany();
+  const categories = await responseCategories;
+  const structuredCategories = fetchCategoriesAmount(categories);
+  return structuredCategories;
+}
+
+async function fetchCategoriesAmount(categoryArr: any[]) {
+  const categoryPromises = categoryArr.map(async (category: any) => {
+    const amount = await prisma.product_category.count({
+      where: {
+        category_id: category.id,
+      },
+    });
+    return {
+      category: category.category_description,
+      link: category.category_name,
+      amount: amount,
+    };
+  });
+
+  const categoryAmount = await Promise.all(categoryPromises);
+  return categoryAmount;
+}
+
+export default async function ShopLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const dbCategories = await fetchCategories();
+
   return (
     <main className={styles.main}>
-      <ShopMenu categories={categories} />
+      <ShopMenu categories={dbCategories} />
       {children}
     </main>
   );
