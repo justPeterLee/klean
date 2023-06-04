f23492da44c680a251922c9b4dd63908dda8f977
Peter Lee <PeterLee2094@gmail.com>
2023-06-04 08:12:25 -0500

    query prisma to get all data

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,9 +1,28 @@
 import styles from "../../../styling/Product.module.css";
+import prisma from "../../../../lib/db";
 
 interface Props {
-  params: { productId: number };
+  params: { productId: string };
+}
+
+async function getProductDetail(productId: string) {
+  const productData = await prisma.product.findUnique({
+    where: {
+      id: parseInt(productId),
+    },
+    include: {
+      review: true,
+      technical_point: true,
+      image: true,
+      product_selection: { include: { product_option: true } },
+      product_sku: true,
+    },
+  });
+
+  console.log(productData);
 }
 export default async function ProductDetail({ params }: Props) {
+  const productData = await getProductDetail(params.productId);
   return (
     <div className={styles.main}>
       <p>params: {params.productId}</p>
