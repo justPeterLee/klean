2e3d266289a89cddef635ebfda87fc6f35a5c675
Peter Lee <PeterLee2094@gmail.com>
2023-06-04 07:06:39 -0500

    created product detail page

---
+++ src/app/product/[productId]/page.tsx
@@ -0,0 +1,9 @@
+import styles from "../../../styling/Product.module.css";
+
+export default async function ProductDetail() {
+  return (
+    <div className={styles.main}>
+      <p>product detail</p>
+    </div>
+  );
+}
