03a9e12fe8dc122d0a3b4448d08c0eb1c26c9e27
Peter Lee <PeterLee2094@gmail.com>
2023-06-05 15:17:26 -0500

    creted sub image item

--- src/components/ProductPage/ProductImage.tsx
+++ src/components/ProductPage/ProductImage.tsx
@@ -1,15 +1,51 @@
+"use client";
 import styles from "../../styling/Product.module.css";
 import Image from "next/image";
+import { useState } from "react";
 
 interface ProductImageProps {
   images?: string[];
 }
 
+const imagesArr = [
+  { name: "thumbnail", file: "image 1" },
+  { name: "product-image", file: "image 2" },
+  { name: "product-image", file: "image 3" },
+  { name: "product-image", file: "image 4" },
+  { name: "product-image", file: "image 5" },
+  { name: "product-image", file: "image 6" },
+];
 export default function ProductImage(images: ProductImageProps) {
+  const thumbnail = imagesArr.filter((image) => {
+    return image.name === "thumbnail";
+  })[0].file;
+  const [mainImage, setMainImage] = useState(thumbnail);
+  const readImage = (image: string) => {
+    setMainImage(image);
+  };
+
   return (
     <div className={styles.ImageContainer}>
-      <div className={styles.SubImageContainer}></div>
-      <div className={styles.MainImageContainer}></div>
+      {JSON.stringify(thumbnail)}
+      <div className={styles.SubImageContainer}>
+        <SubImage sendImage={readImage} />
+      </div>
+      <div className={styles.MainImageContainer}>
+        <span>{mainImage}</span>
+      </div>
     </div>
+  );
+}
+
+interface SubImageProps {
+  image?: string;
+  sendImage: (params: string) => void;
+}
+
+function SubImage(props: SubImageProps) {
+  return (
+    <button className={styles.SubImageButton}>
+      <span className={styles.SubImageImg}></span>
+    </button>
   );
 }
