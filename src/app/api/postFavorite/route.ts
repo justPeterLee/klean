49e3d09832e6000998900e17096aca5d7c614ea3
Peter Lee <PeterLee2094@gmail.com>
2023-06-15 18:41:29 -0500

    post request to create favorite

---
+++ src/app/api/postFavorite/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "../../../../lib/db";
+export async function POST(req: any) {
+  //   const request = req;
+  const data = await req.json();
+
+  await prisma.favorite.create({
+    data: {
+      product_id: data.productId,
+      user_id: parseInt(data.userId),
+    },
+  });
+
+  return new NextResponse(JSON.stringify({ hello: "world" }), {
+    headers: { "content-type": "application/json" },
+  });
+}
