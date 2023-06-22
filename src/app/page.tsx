f196efa5a1ccd80918e3d1856f9c1bb49f32ad79
Peter Lee <PeterLee2094@gmail.com>
2023-06-22 15:36:31 -0500

    created updated store carusel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,30 @@ import {
   SideFeature,
   ContactButton,
 } from "@/components/Feature/Feature";
-import { StoreFeature } from "@/components/Feature/FeatureStoreItem/FeatureStoreItem";
+import {
+  StoreFeature,
+  StoreCarousel,
+} from "@/components/Feature/FeatureStoreItem/FeatureStoreItem";
 const items = [
   { id: 1, product_name: "product_1", product_image: "image", price: "$100" },
-  { id: 2, product_name: "product_1", product_image: "image", price: "$100" },
-  { id: 3, product_name: "product_1", product_image: "image", price: "$100" },
-  { id: 4, product_name: "product_1", product_image: "image", price: "$100" },
-  { id: 5, product_name: "product_1", product_image: "image", price: "$100" },
+  { id: 2, product_name: "product_2", product_image: "image", price: "$100" },
+  { id: 3, product_name: "product_3", product_image: "image", price: "$100" },
+  { id: 4, product_name: "product_4", product_image: "image", price: "$100" },
+  { id: 5, product_name: "product_5", product_image: "image", price: "$100" },
+];
+
+const itemss = [
+  { id: 1, name: "product_1", image: "image", price: "$100" },
+  { id: 2, name: "product_2", image: "image", price: "$100" },
+  { id: 3, name: "product_3", image: "image", price: "$100" },
+  { id: 4, name: "product_4", image: "image", price: "$100" },
+  { id: 5, name: "product_5", image: "image", price: "$100" },
+  { id: 6, name: "product_6", image: "image", price: "$100" },
+  { id: 7, name: "product_7", image: "image", price: "$100" },
+  { id: 8, name: "product_8", image: "image", price: "$100" },
+  { id: 9, name: "product_9", image: "image", price: "$100" },
+  { id: 10, name: "product_10", image: "image", price: "$100" },
+  { id: 11, name: "product_11", image: "image", price: "$100" },
 ];
 
 const product_info1 = {
@@ -25,6 +42,7 @@ export default function Home() {
     <main className={styles.main}>
       <MainFeature />
       <StoreFeature caruselItem={items} />
+      <StoreCarousel data={itemss} />
       <SideFeature product_info={product_info1} />
       <SideFeature
         product_info={product_info1}
--- src/components/Feature/FeatureStoreItem/FeatureStoreItem.tsx
+++ src/components/Feature/FeatureStoreItem/FeatureStoreItem.tsx
@@ -86,3 +86,99 @@ export function StoreFeature(props: CaruselItem) {
     </div>
   );
 }
+
+interface StoreCarouselProps {
+  data: { id: number; name: string; price: string; image: string }[];
+}
+export function StoreCarousel(props: StoreCarouselProps) {
+  const { data } = props;
+  const transitionPerItem = 16.8;
+  const maxClick = Math.floor(data.length / 3);
+  const minClick = 0;
+  const lastClick =
+    (data.length - Math.floor(data.length / 3) * 3) * transitionPerItem; // multpily by per trans (33.6)
+
+  const [clickPos, setClickPos] = useState(0);
+  const [transitionPos, setTransitionPos] = useState(0);
+
+  const moveLeft = () => {
+    if (clickPos > minClick) {
+      setClickPos(clickPos - 1);
+      if (clickPos === minClick + 1) {
+        setTransitionPos(0);
+      } else {
+        setTransitionPos(transitionPos - transitionPerItem * 3);
+      }
+    }
+  };
+
+  const moveRight = () => {
+    if (clickPos < maxClick) {
+      setClickPos(clickPos + 1);
+      if (clickPos === maxClick - 1) {
+        setTransitionPos(transitionPos + lastClick);
+      } else {
+        setTransitionPos(transitionPos + transitionPerItem * 3);
+      }
+    }
+  };
+
+  return (
+    <div className={styles.CaruselContainer}>
+      <button
+        className={styles.CaruselButton}
+        style={{ left: "-5rem" }}
+        onClick={moveLeft}
+      >
+        {"<"}
+      </button>
+      <div className={styles.CaruselDisplay}>
+        <div
+          className={`${styles.CaruselDisplaySub}`}
+          style={{ transform: `translateX(-${transitionPos}rem)` }}
+        >
+          {data.length ? (
+            data.map((item) => (
+              <StoreCarouselItem
+                key={item.id}
+                id={item.id}
+                name={item.name}
+                price={item.price}
+                image={item.image}
+              />
+            ))
+          ) : (
+            <p>no items</p>
+          )}
+        </div>
+      </div>
+      <button
+        className={styles.CaruselButton}
+        style={{ right: "-5rem" }}
+        onClick={moveRight}
+      >
+        {">"}
+      </button>
+    </div>
+  );
+}
+
+interface StoreCarouselItem {
+  id: number;
+  name: string;
+  price: string;
+  image: string;
+}
+function StoreCarouselItem(props: StoreCarouselItem) {
+  const { id, name, price, image } = props;
+
+  return (
+    <div className={styles.StoreCarouselItemContainer}>
+      <div className={styles.CaruselItemImage}>{image}</div>
+      <span className={styles.CaruselItemInfo}>
+        <p>{name}</p>
+        <p>{price}</p>
+      </span>
+    </div>
+  );
+}
