5e1f4a04aeda0668d0a6722936c34e2af0569ba9
Peter <Peterlee2094@gmail.com>
2023-07-19 13:30:51 -0500

    removed  unused POST route

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 import styles from "../../styling/Contact.module.css";
+
+// components
 import { ContactForm, ContactDesc } from "@/components/ContactPage/ContactForm";
 
 export default function Contact() {
-  /* POST("asdf"); */
-  const sendEmail = async (emailData: any) => {};
   const readEmailData = async (emailData: any) => {
     console.log(emailData);
 
-    const req = await fetch("/api/contact/recieveEmail", {
+    await fetch("/api/contact/recieveEmail", {
       method: "POST",
       body: JSON.stringify(emailData),
       headers: {
@@ -16,6 +16,7 @@ export default function Contact() {
       },
     });
   };
+
   return (
     <main className={styles.main}>
       <ContactForm sendEmailData={readEmailData} />
