1a5bae1ed08f7d3ddca39d5625fc734402f84441
Peter <Peterlee2094@gmail.com>
2023-07-19 14:15:23 -0500

    removed extra fetch

--- src/app/shop/[category]/page.tsx
+++ src/app/shop/[category]/page.tsx
@@ -31,31 +31,23 @@ async function getItems(params: string) {
     },
   });
 
-  const products = productData.map((product: any) => {
-    const thumbnail = product.product_ref.image.filter((image: any) => {
-      return image.image_name === "thumbnail";
-    });
-
-    return {
-      id: product.product_ref.id,
-      name: product.product_ref.product_name,
-      price: product.product_ref.product_price,
-      image: product.product_ref.image[0].image_file,
-    };
-  });
+  const products = await Promise.all(
+    productData.map(async (product: any) => {
+      const image = await fetchImage(product.product_ref.image[0].image_file);
+      return {
+        id: product.product_ref.id,
+        name: product.product_ref.product_name,
+        price: product.product_ref.product_price,
+        imageUrl: image,
+      };
+    })
+  );
 
   return products;
 }
 
-
 export default async function ShopCategory({ params }: Props) {
   const productItems = await getItems(params.category);
 
-  const productImage = await Promise.all(
-    productItems.map(async (product: any) => {
-      const image = await fetchImage(product.image);
-      return { ...product, imageUrl: image };
-    })
-  );
-  return <ShopItemDisplay items={productImage} />;
+  return <ShopItemDisplay items={productItems} />;
 }
