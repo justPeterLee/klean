84bc1c31b1858e896df7d6012b7de87f783bdf52
Peter <Peterlee2094@gmail.com>
2023-07-28 12:19:24 -0500

    fetch github account

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,28 +1,27 @@
-"use client";
 import styles from "../../styling/About.module.css";
-import { useState } from "react";
-
-// components
-import Menu from "@/components/GlobalComponent/Menu/Menu";
-import {
-  CommentBlock,
-  TeamHeadShot,
-} from "@/components/AboutPage/CommentBlock";
-
-const text1 =
-  "Welcome to Klean, the premier destination for computer mice enthusiasts seeking high-quality and unique products. At Klean, we understand the importance of a reliable and comfortable mouse for your computing needs. That's why we have curated a remarkable collection of top-notch computer mice that combine exceptional performance, ergonomic design, and cutting-edge technology. Whether you're a gamer, designer, or professional, Klean is committed to delivering an unparalleled browsing and gaming experience through our carefully selected range of mice.";
-const text2 =
-  "At Klean, we believe that every user deserves a mouse that perfectly suits their preferences and enhances their productivity. Our extensive selection features a wide variety of styles, shapes, and sizes, ensuring that you'll find the ideal mouse for your unique needs. From sleek and minimalistic designs to vibrant and eye-catching models, our range caters to diverse tastes and aesthetics. We partner with renowned manufacturers who share our passion for quality and innovation, ensuring that each mouse you find at Klean is crafted to the highest standards.";
-const text3 =
-  "What sets Klean apart is our unwavering commitment to offering exceptional customer service and satisfaction. Our knowledgeable team is dedicated to guiding you through your shopping journey, providing expert advice and answering any questions you may have. We take pride in our attention to detail and personalized approach, striving to create a seamless and enjoyable shopping experience for all our valued customers. From browsing our user-friendly website to receiving your meticulously packaged order, Klean ensures that every step of your journey reflects our commitment to excellence.";
-const text4 =
-  "With Klean, you can shop with confidence, knowing that each mouse in our collection undergoes rigorous quality assurance checks. We prioritize functionality, durability, and performance, ensuring that our products withstand the demands of daily use. Our dedication to quality extends beyond the physical aspects of our mice, as we also prioritize software compatibility, ergonomic comfort, and customizable features to enhance your overall experience. Discover the world of exceptional computer mice at Klean and elevate your computing setup to new heights.";
+import { AboutPage } from "@/components/AboutPage/CommentBlock";
 
 const comments = [
-  { title: "", comment: text1 },
-  { title: "", comment: text2 },
-  { title: "", comment: text3 },
-  { title: "", comment: text4 },
+  {
+    title: "",
+    comment:
+      "Welcome to Klean, the premier destination for computer mice enthusiasts seeking high-quality and unique products. At Klean, we understand the importance of a reliable and comfortable mouse for your computing needs. That's why we have curated a remarkable collection of top-notch computer mice that combine exceptional performance, ergonomic design, and cutting-edge technology. Whether you're a gamer, designer, or professional, Klean is committed to delivering an unparalleled browsing and gaming experience through our carefully selected range of mice.",
+  },
+  {
+    title: "",
+    comment:
+      "At Klean, we believe that every user deserves a mouse that perfectly suits their preferences and enhances their productivity. Our extensive selection features a wide variety of styles, shapes, and sizes, ensuring that you'll find the ideal mouse for your unique needs. From sleek and minimalistic designs to vibrant and eye-catching models, our range caters to diverse tastes and aesthetics. We partner with renowned manufacturers who share our passion for quality and innovation, ensuring that each mouse you find at Klean is crafted to the highest standards.",
+  },
+  {
+    title: "",
+    comment:
+      "What sets Klean apart is our unwavering commitment to offering exceptional customer service and satisfaction. Our knowledgeable team is dedicated to guiding you through your shopping journey, providing expert advice and answering any questions you may have. We take pride in our attention to detail and personalized approach, striving to create a seamless and enjoyable shopping experience for all our valued customers. From browsing our user-friendly website to receiving your meticulously packaged order, Klean ensures that every step of your journey reflects our commitment to excellence.",
+  },
+  {
+    title: "",
+    comment:
+      "With Klean, you can shop with confidence, knowing that each mouse in our collection undergoes rigorous quality assurance checks. We prioritize functionality, durability, and performance, ensuring that our products withstand the demands of daily use. Our dedication to quality extends beyond the physical aspects of our mice, as we also prioritize software compatibility, ergonomic comfort, and customizable features to enhance your overall experience. Discover the world of exceptional computer mice at Klean and elevate your computing setup to new heights.",
+  },
 ];
 
 const team = [
@@ -31,53 +30,47 @@ const team = [
   { image: "", name: "Name name", role: "Developer" },
   { image: "", name: "Name name", role: "Developer" },
 ];
-export default function About() {
-  const [optionSelected, setOptionSelected] = useState("About Us");
 
-  const readOption = (option: string) => {
-    setOptionSelected(option);
-  };
+async function fetchTeam() {
+  const teamData = process.env.GITHUB_TOKEN;
+  try {
+    let team = await JSON.parse(teamData!);
+    const userName = Object.keys(team);
+
+    const data = await Promise.all(
+      userName.map(async (user: string) => {
+        const role = team[user].role;
+        const accessToken = team[user].key;
+        const apiUrl = `https://api.github.com/users/${user}`;
+        const headers = {
+          Authorization: `token ${accessToken}`,
+        };
+        const res = await fetch(apiUrl, { headers });
+
+        const data = await res.json();
+
+        return {
+          name: data.name,
+          image: data.avatar_url,
+          role: role,
+          link: data.html_url,
+        };
+      })
+    );
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+  // const res = await fetch()
+}
+
+export default async function About() {
+  const team: any = await fetchTeam();
+
   return (
     <main className={` ${styles.container}`}>
-      {/* Menu */}
-      <div className={styles.menuContainer}>
-        <Menu
-          options={["About Us", "Our Team"]}
-          readOption={readOption}
-          selected={optionSelected}
-          containerStyle={{
-            top: "0",
-            left: "0",
-            backgroundColor: "rgb(0, 0, 0, 0.03)",
-          }}
-          hover={"rgb(0, 0, 0, 0.1)"}
-        />
-      </div>
-
-      {/* About Us container */}
-      {optionSelected === "About Us" && (
-        <div className={styles.aboutContainer}>
-          {comments &&
-            comments.map((comment, index) => (
-              <CommentBlock key={index} comment={comment.comment} />
-            ))}
-        </div>
-      )}
-
-      {/* Our Team container */}
-      {optionSelected === "Our Team" && (
-        <div className={styles.teamContainer}>
-          {team &&
-            team.map((person, index) => (
-              <TeamHeadShot
-                key={index}
-                image={person.image}
-                name={person.name}
-                role={person.role}
-              />
-            ))}
-        </div>
-      )}
+      <AboutPage comments={comments} team={team} />
     </main>
   );
 }
--- src/components/AboutPage/CommentBlock.tsx
+++ src/components/AboutPage/CommentBlock.tsx
@@ -1,14 +1,18 @@
 "use client";
 import styles from "../../styling/About.module.css";
+import { useState } from "react";
+import Menu from "@/components/GlobalComponent/Menu/Menu";
+import { trace } from "console";
 
 interface CommentBlockProps {
   title?: string;
   comment: string;
 }
 interface TeamHeadShot {
-  image?: string;
+  image: string;
   name: string;
   role: string;
+  link: string;
 }
 
 export function CommentBlock(props: CommentBlockProps) {
@@ -22,14 +26,83 @@ export function CommentBlock(props: CommentBlockProps) {
 }
 
 export function TeamHeadShot(props: TeamHeadShot) {
-  const { image, name, role } = props;
+  const { image, name, role, link } = props;
   return (
     <div className={styles.headShotContainer}>
-      <div className={styles.teamImage}></div>
+      <img className={styles.teamImage} src={image} alt={name} />
       <div className={styles.teamInfo}>
-        <p className={styles.teamName}>{name}</p>
+        <p
+          className={styles.teamName}
+          onClick={() => {
+            window.open(link, "_blank");
+          }}
+        >
+          {name}
+        </p>
         <p className={styles.teamRole}>{role}</p>
       </div>
     </div>
+  );
+}
+
+interface AboutPageProps {
+  comments: { title: string; comment: string }[];
+  team: {
+    link: string;
+    image: string;
+    name: string;
+    role: string;
+  }[];
+}
+export function AboutPage(props: AboutPageProps) {
+  const { comments, team } = props;
+  const [optionSelected, setOptionSelected] = useState("About Us");
+
+  const readOption = (option: string) => {
+    setOptionSelected(option);
+  };
+  return (
+    <main className={` ${styles.container}`}>
+      {/* Menu */}
+      <div className={styles.menuContainer}>
+        <Menu
+          options={["About Us", "Our Team"]}
+          readOption={readOption}
+          selected={optionSelected}
+          containerStyle={{
+            top: "0",
+            left: "0",
+            backgroundColor: "rgb(0, 0, 0, 0.03)",
+          }}
+          hover={"rgb(0, 0, 0, 0.1)"}
+        />
+      </div>
+
+      {/* About Us container */}
+      {optionSelected === "About Us" && (
+        <div className={styles.aboutContainer}>
+          {comments &&
+            comments.map((comment, index) => (
+              <CommentBlock key={index} comment={comment.comment} />
+            ))}
+        </div>
+      )}
+
+      {/* Our Team container */}
+      {optionSelected === "Our Team" && (
+        <div className={styles.teamContainer}>
+          {team &&
+            team.map((person, index) => (
+              <TeamHeadShot
+                key={index}
+                image={person.image}
+                name={person.name}
+                role={person.role}
+                link={person.link}
+              />
+            ))}
+        </div>
+      )}
+    </main>
   );
 }
